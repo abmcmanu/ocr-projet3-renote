175f0cb6a5a12b2362c0048cec403d9a289f15e7
abmcmanu <manueolinga@gmail.com>
2026-03-22 15:21:26 +0100

    [setup:react] Setup React + Zustand dans Laravel

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import '../css/app.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './views/App';
+
+const root = document.getElementById('app');
+
+if (root) {
+    createRoot(root).render(<App />);
+}
